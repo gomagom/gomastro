f9369460e61020b49a32dafeecffa2ebb47180e2
gomagom <peanut.kaki@gmail.com>
2024-04-28 06:41:11 +0900

    テスト

--- src/library/strapi.ts
+++ src/library/strapi.ts
@@ -29,7 +29,7 @@ export async function fetchApi<T>({
   if (endpoint.startsWith('/')) {
     endpoint = endpoint.slice(1)
   }
-
+  console.log(import.meta.env.STRAPI_URL)
   const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`)
   const headers = new Headers()
   headers.set('Authorization', `Bearer ${import.meta.env.STRAPI_TOKEN}`)
